6a807eedd3b4c4596f07103739359227a4d743fb
StevenCuasqui <cuasquisteven@gmail.com>
2018-10-31 16:07:53 -0500

    promesas

--- 01-Javascript/07-callback-propio.js
+++ 01-Javascript/07-callback-propio.js
@@ -1,84 +1,136 @@
-const fs = require('fs')
-let contenidofinal = 'Inicial'
-function appendFile(nombreArchivo,contenidoArchivo,callback){
-    fs.readFile(nombreArchivo,'utf-8',(error,contenidoLeido)=>{
-            if(error){
-                const contenido = contenidoArchivo
-                fs.writeFile(nombreArchivo, contenido,
-                    (err)=>{
-                        if(err){
-                            callback(err)
-                        }else{
-                            callback(undefined,contenido)
+const fs = require('fs');
 
+let contenidoFinal = 'Inicial';
+
+function appendFile(nombreArchivo,
+                    contenidoArchivo,
+                    callback) {
+    // 1 -> leer archivo
+    // 2.1 -> Concatenams contenido
+    // 2.2 -> Creamos el archivo
+    fs.readFile(
+        nombreArchivo,
+        'utf-8',
+        (error, contenidoLeido) => {
+            if (error) {
+                const contenido = contenidoArchivo;
+                fs.writeFile(
+                    nombreArchivo,
+                    contenido,
+                    (err) => {
+                        if (err) {
+                            callback(err);
+                        } else {
+                            callback(undefined, contenido);
                         }
                     }
-                )
-            }else{
-                const contenido=contenidoLeido+contenidoArchivo
-                fs.writeFile(nombreArchivo, contenido,
-                    (err)=>{
-                        if(err){
-                            callback(err)
-                        }else{
-                            callback(undefined,contenido)
-
+                );
+            } else {
+                const contenido = contenidoLeido + contenidoArchivo;
+                fs.writeFile(
+                    nombreArchivo,
+                    contenido,
+                    (err) => {
+                        if (err) {
+                            callback(err);
+                        } else {
+                            callback(undefined, contenido);
                         }
                     }
-                )
+                );
             }
-
         }
-    )
+    );
 }
 
-appendFile('07-texto.txt','\nAdios',(error,contenidoTexto)=>{
-    if(error){
-        console.log(error)
-    } else{
-        console.log(contenidoTexto)
+appendFile(
+    '07-texto.txt',
+    '\nAdios',
+    (error, contenidoTexto) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(contenidoTexto)
+        }
     }
-})
+);
 
+// ['A','B','C']
 
-//['A','B','C']
-//0-A.txt 'A'
-//1-B.txt   'B'
-//2-C.txt   'C'
+// 0-A.txt  'A'
+// 1-B.txt  'B'
+// 2-C.txt  'C'
 
-const respuesta={
-    nombreArchivo:'',
-    contenidoArchivo:'',
+const respuesta = {
+    nombreArchivo: '0-A.txt',
+    contenidoArchivo: 'A',
     error: undefined
-}
+};
 
-function ejercicio(arregloStrings,callback){
+// [respuesta, respuesta, respuesta, respuesta]
+
+
+function ejercicio(
+    arregloStrings,
+    callback) {
     const arregloRespuestas = [];
-    arregloStrings.forEach(
-        (string, indice)=>{
-            const nombreArchivo = `${indice}-${string}.txt`;
-            const contenidoArchivo = string;
 
-            fs.writeFile(nombreArchivo, contenidoArchivo,
-                (err)=>{
-                    const respuesta={
-                        nombreArchivo:nombreArchivo,
-                        contenidoArchivo:contenidoArchivo,
-                        error: err
+    arregloStrings
+        .forEach(
+            (string, indice) => {
+                const nombreArchivo = `${indice}-${string}.txt`;
+                const contenidoArchio = string;
+
+                fs.writeFile(
+                    nombreArchivo,
+                    contenidoArchio,
+                    (err) => {
+                        const respuesta = {
+                            nombreArchivo: nombreArchivo,
+                            contenidoArchivo: contenidoArchio,
+                            error: err
+                        };
+                        arregloRespuestas.push(respuesta);
+                        const terminoElArreglo = arregloStrings.length === arregloRespuestas.length;
+                        if (terminoElArreglo) {
+                            callback(arregloRespuestas);
+                        }
+
                     }
-                    arregloRespuestas.push(respuesta)
-                    const terminoElArreglo = arregloStrings.length == arregloRespuestas.length;
-                    if(terminoElArreglo){
-                        callback(arregloRespuestas);
-                    }
+                )
+
             }
-            )
+        );
 
+    /*
+    for (let i = 0; i < arregloStrings.length; i++) {
+        fs.writeFile(
+            `${i}-${arregloStrings[i]}.txt`,
+            arregloStrings[i],
+            function (err) {
+                const respuesta = {
+                    nombreArchivo: `${i}-${arregloStrings[i]}.txt`,
+                    contenidoArchivo: arregloStrings[i],
+                    error: err
+                };
+                arregloRespuestas.push(respuesta);
+                const terminoElArreglo = arregloStrings.length === arregloRespuestas.length;
+                if (terminoElArreglo) {
+                    callback(arregloRespuestas);
+                }
+            }
+        )
+    }
+    */
 
-        }
-    )
 }
 
-ejercicio(['A','B','c'],(arregloRespuestas)=>{
-    console.log(arregloRespuestas);
-});
+
+ejercicio(
+    ['A', 'B', 'C'],
+    (arregloRespuestas) => {
+        console.log(arregloRespuestas);
+    }
+);
+
+//LA TRANSFORMACION EN PROMESA EN 08-PROMESAS
--- 01-Javascript/08-promesas.js
+++ 01-Javascript/08-promesas.js
@@ -18,8 +18,7 @@ const promesa = (nombreArchivo) => {
     );
 };
 
-const promesaEscritura = (
-    nombreArchivo,
+const promesaEscritura = ( nombreArchivo,
     contenidoArchivo) => {
     return new Promise(
         (resolve, reject) => {
@@ -59,3 +58,45 @@ promesa('099-texto.txt')
             console.log('Mal', error);
         }
     );
+//Transformacion
+
+const nombreArchivo= '07-text.txt'
+const contenido= 'Hola'
+const appendFile=(nombreArchivo,contenido)=>{
+    return new Promise(
+        (resolve,reject)=>{
+            fs.readFile(
+                nombreArchivo,
+                'utf-8',
+                (error, contenido) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(contenido);
+                    }
+                }
+            );
+        }
+    )
+}
+
+
+
+appendFile(nombreArchivo,contenido)
+.then(
+fs.writeFile(
+    nombreArchivo,
+    contenido,
+    (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(contenido)
+        }
+    }
+)
+)
+
+.catch(
+    console.log('Error')
+)
