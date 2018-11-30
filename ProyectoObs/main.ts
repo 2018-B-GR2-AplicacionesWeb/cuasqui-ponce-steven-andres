a400abba00f1c18749e79addedd9ad875e8db82e
StevenCuasqui <cuasquisteven@gmail.com>
2018-11-30 04:39:31 -0500

    Commit proyecto

---
+++ ProyectoObs/main.ts
@@ -0,0 +1,50 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const rxjs = require('rxjs');
+const timer = require('rxjs').timer;
+const mergeMap = require('rxjs/operators').mergeMap;
+const map = require('rxjs/operators').map;
+const retryWhen = require('rxjs/operators').retryWhen;
+const delayWhen = require('rxjs/operators').delayWhen;
+
+function inicialiarBDD() {
+
+    return new Promise(
+        (resolve, reject) => {
+            fs.readFile(
+                'bdd.json',
+                'utf-8',
+                (error, contenidoArchivo) => { // CALLBACK
+                    if (error) {
+
+                        fs.writeFile(
+                            'bdd.json',
+                            '{"usuarios":[],"mascotas":[]}',
+                            (error) => {
+                                if (error) {
+                                    reject({
+                                        mensaje: 'Error creando',
+                                        error: 500
+                                    })
+                                } else {
+                                    resolve({
+                                        mensaje: 'BDD leida',
+                                        bdd: JSON.parse('{"usuarios":[],"mascotas":[]}')
+                                    })
+                                }
+
+                            }
+                        )
+
+                    } else {
+                        resolve({
+                            mensaje: 'BDD leida',
+                            bdd: JSON.parse(contenidoArchivo)
+                        })
+                    }
+                }
+            )
+        }
+    );
+
+}
