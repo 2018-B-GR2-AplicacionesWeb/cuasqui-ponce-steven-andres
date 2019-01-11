1e48ff474f34eb006507cf5b2c3c2b7ea2881e65
StevenCuasqui <cuasquisteven@gmail.com>
2019-01-11 00:42:43 -0500

    ejs

---
+++ 03-http/videos/src/app.controller.ts
@@ -0,0 +1,128 @@
+import {
+    Get,
+    Controller,
+    Request,
+    Response,
+    Headers,
+    HttpCode,
+    HttpException,
+    Query,
+    Param,
+    Res,
+    Post
+} from '@nestjs/common';
+import {AppService} from './app.service';
+import {Observable, of} from "rxjs";
+
+// http://192.168.1.2:3000/Usuario/saludar     METODO -> GET
+// http://192.168.1.2:3000/Usuario/salir   METODO -> POST
+// http://192.168.1.2:3000/Usuario/registrar METODO -> PUT
+// http://192.168.1.2:3000/Usuario/borrar METODO -> DELETE
+// http://192.168.1.2:3000/Notas
+
+
+// Decorador -> FUNCION
+// SE EJECUTA ANTES DE ALGO
+@Controller('Usuario') // Decoradores!
+export class AppController {
+
+    usuarios = [
+        {
+            nombre: 'Adrian',
+            id: 1
+        },
+        {
+            nombre: 'Vicente',
+            id: 2
+        },
+        {
+            nombre: 'Carolina',
+            id: 3
+        }
+    ];
+
+    @Get('saludar')
+    saludar(
+        @Query() queryParams,
+        @Query('nombre') nombre,
+        @Headers('seguridad') seguridad,
+    ): string { // metodo!
+        return nombre;
+    }
+
+    // /Usuario/segmentoUno/12/segmentoDos
+    @Get('segmentoUno/:idUsuario/segmentoDos')
+    ruta(
+        @Param() todosParametrosRuta,
+        @Param('idUsuario') idUsuario,
+    ): string { // metodo!
+        return idUsuario;
+    }
+
+
+
+
+
+
+
+
+
+
+    @Get('despedirse')
+    @HttpCode(201)
+    despedirse(): Promise<string> {
+        return new Promise<string>(
+            (resolve, reject) => {
+
+                throw new HttpException({
+                        mensaje: 'Error en despedirse',
+                    },
+                    400);
+            }
+        );
+    }
+
+    @Get('tomar')
+    @HttpCode(201)
+    tomar(): string { // metodo!
+        return 'Estoy borracho';
+    }
+
+    @Get('saludarObservable')
+    saludarObservable(): Observable<string> { // metodo!
+        return of('Hola mundo');
+    }
+
+        @Get('inicio')
+    inicio(
+        @Res() response
+    ) {
+        response.render('inicio', {
+            nombre: 'Adrian',
+            arreglo: this.usuarios
+        });
+    }
+
+    @Post('borrar/:idUsuario')
+    borrar(
+        @Param('idUsuario') idUsuario,
+        @Res() response
+    ) {
+        const indiceUsuario = this
+            .usuarios
+            .findIndex(
+                (usuario) => usuario.id === Number(idUsuario)
+            );
+
+        this.usuarios.splice(indiceUsuario,1);
+
+
+        response.render('inicio', {
+            nombre: 'Adrian',
+            arreglo: this.usuarios
+        });
+    }
+
+
+
+}
---
+++ 03-http/videos/src/main.ts
@@ -0,0 +1,25 @@
+// const NestFactory = require('@nestjs/core').NestFactory; //js
+import {NestFactory} from '@nestjs/core'; // ts
+// import * as httpserver from 'http-server'; // js
+import {Options} from 'http-server'; // js
+import {AppModule} from './app.module';
+//import {} from './mi-codigo';
+//const a = require('./mi-codigo').a;
+
+async function bootstrap() {
+    //console.log(a)
+    const app = await NestFactory.create(AppModule);
+    app.set('view engine','ejs');
+    await app.listen(3000);
+}
+
+bootstrap();
+
+// of([1,2,3,4,5])
+// ->map(
+//     ([1,2,3,4,5])=>{
+//         [1,2,3,4,5].findIndex(3)
+//             [1,2,3,4,5][3].asdasdasdasd
+//         return [1,2,3,4,5]
+//     }
+// )
